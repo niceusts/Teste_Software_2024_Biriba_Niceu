78cd77dc001f87f830d83a741cec8ae2acb76f36
Niceu Santos Biriba <niceusantos1365@gmail.com>
2024-08-05 08:11:32 -0300

    Add project files.

---
+++ Application/StreamHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Teste_Software_2024_Biriba_Niceu
+{
+    public static class StreamHelper
+    {
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0; // Reposiciona o stream para o início
+            return stream;
+        }
+    }
+}
---
+++ TestProject/StreamHelperTest.cs
@@ -0,0 +1,30 @@
+﻿using Teste_Software_2024_Biriba_Niceu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace Teste_Software_2024_Biriba_Niceu.Tests
+{
+    [TestClass]
+    public class StreamHelperTests
+    {
+        [TestMethod]
+        public void GenerateStreamFromString_ShouldReturnStreamContainingInputString()
+        {
+            // Arrange
+            string input = "a,b \n c,d";
+            var expected = Encoding.UTF8.GetBytes(input);
+
+            // Act
+            using (var stream = StreamHelper.GenerateStreamFromString(input))
+            {
+                // Assert
+                using (var reader = new BinaryReader(stream))
+                {
+                    byte[] result = reader.ReadBytes((int)stream.Length);
+                    CollectionAssert.AreEqual(expected, result);
+                }
+            }
+        }
+    }
+}
